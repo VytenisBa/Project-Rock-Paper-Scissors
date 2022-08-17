07123cb9fc599fe09ca07c3499bb730af24055ba
VytenisBa <vbgithub@gmail.com>
2022-08-17 21:02:58 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,59 +1,48 @@
-//selectors: surisa html su js'u. paima reiksmes pagal id ir klases
+
 const playerText = document.querySelector("#playerText");
 const computerText = document.querySelector("#computerText");
 const resultText = document.querySelector("#resultText");
-
 const choiceBtns = document.querySelectorAll(".choiceBtn");
-
-//variables: kintamieji, kuriuose bus saugomi "pasirinkimai"
 let player;
 let computer;
 let result;
 
-
-//arow function: 
-//array.forEach(function(currentValue, index, arr), thisValue)
-//document.addEventListener(event, function, Capture)
-//
 choiceBtns.forEach(button => button.addEventListener("click", () => {
+
     player = button.textContent;
     computerTurn();
-    playerText.textContent = "Player: ${player}";
-    computerText.textContent = "Computer: ${computer}";
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
     resultText.textContent = checkWinner();
-
-
-} ));
-
+}));
 
 function computerTurn(){
+
     const randNum = Math.floor(Math.random() * 3) + 1;
 
     switch(randNum){
-        case 1:
-            computer = "Rock"
-            break
-        case 2:
-            computer = "Paper"
-            break
-        case 3:
-            computer = "Scissors"
-            break
+      case 1:
+        computer = "ROCK";
+        break;
+      case 2:
+        computer = "PAPER";
+        break;
+      case 3:
+        computer = "SCISSORS";
+        break;
     }
-   
 }
-
 function checkWinner(){
-    if (player == computer){
-        return "Draw!"
+    if(player == computer){
+      return "Draw!";
     }
-    else if (computer == "Rock"){
-        return (player == "Paper") ? "You Win!" : "You Lose!"
+    else if(computer == "ROCK"){
+      return (player == "PAPER") ? "You Win!" : "You Lose!"
     }
-    else if (computer == "Paper"){
-        return (player == "Scissors") ? "You Win!" : "You Lose!"
+    else if(computer == "PAPER"){
+      return (player == "SCISSORS") ? "You Win!" : "You Lose!"
     }
-    else if (computer == "Scissors"){
-        return (player == "Rock") ? "You Win!" : "You Lose!"
+    else if(computer == "SCISSORS"){
+      return (player == "ROCK") ? "You Win!" : "You Lose!"
     }
 }
