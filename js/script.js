e183b8c62ed001552972e33b96c6714f54daaf42
VytenisBa <vbgithub@gmail.com>
2022-09-04 20:30:58 +0300

    by the book

--- js/script.js
+++ js/script.js
@@ -1,59 +1,77 @@
-//selectors: surisa html su js'u. paima reiksmes pagal id ir klases
-const playerText = document.querySelector("#playerText");
-const computerText = document.querySelector("#computerText");
-const resultText = document.querySelector("#resultText");
+const options = ['rock', 'paper', 'scissors'];
 
-const choiceBtns = document.querySelectorAll(".choiceBtn");
-
-//variables: kintamieji, kuriuose bus saugomi "pasirinkimai"
-let player;
-let computer;
-let result;
-
-
-//arow function: 
-//array.forEach(function(currentValue, index, arr), thisValue)
-//document.addEventListener(event, function, Capture)
-//
-choiceBtns.forEach(button => button.addEventListener("click", () => {
-    player = button.textContent;
-    computerTurn();
-    playerText.textContent = "Player: ${player}";
-    computerText.textContent = "Computer: ${computer}";
-    resultText.textContent = checkWinner();
-
-
-} ));
-
-
-function computerTurn(){
-    const randNum = Math.floor(Math.random() * 3) + 1;
-
-    switch(randNum){
-        case 1:
-            computer = "Rock"
-            break
-        case 2:
-            computer = "Paper"
-            break
-        case 3:
-            computer = "Scissors"
-            break
-    }
-   
+function getComputerChoice(){
+    const choice = options[Math.floor(Math.random() * options.length)];
+    return choice;
 }
 
-function checkWinner(){
-    if (player == computer){
-        return "Draw!"
+function checkWinner(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        return 'Tie';
     }
-    else if (computer == "Rock"){
-        return (player == "Paper") ? "You Win!" : "You Lose!"
+    else if(
+        (playerSelection == 'rock' && computerSelection == 'scissors')||
+        (playerSelection == 'paper' && computerSelection == 'rock')||
+        (playerSelection == 'scissors' && computerSelection == 'paper')
+    ){
+        return 'Player';
     }
-    else if (computer == "Paper"){
-        return (player == "Scissors") ? "You Win!" : "You Lose!"
-    }
-    else if (computer == "Scissors"){
-        return (player == "Rock") ? "You Win!" : "You Lose!"
+    else {
+        return 'Computer';
     }
 }
+function playRound(playerSelection, computerSelection){
+    const result= checkWinner(playerSelection, computerSelection);
+    if(result == 'Tie'){
+        return "It's a Tie!";
+    }
+    else if(result == 'Player'){
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else{
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+function getPlayerChoice (){
+    let validatedInput = false;
+    while(validatedInput == false){
+        const choice = prompt('Rock, Paper or Scissors?')
+        if(choice == null){
+            continue;
+        }
+        const choiceInLower = choice.toLocaleLowerCase();
+        if(options.includes(choiceInLower)){
+            validatedInput = true;
+            return choiceInLower;
+        }
+    }
+}
+function game(){
+    let scorePlayer = 0;
+    let scoreComputer = 0;
+    console.log('Welcome');
+    for(let i = 0; i < 5; i++){
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+        console.log('---------');
+        if(checkWinner(playerSelection, computerSelection) == 'Player'){
+            scorePlayer++
+        }
+        else if(checkWinner(playerSelection, computerSelection)== 'Computer'){
+            scoreComputer++
+        }
+    }
+
+    console.log('Game Over');
+    console.log(`Score: You ${scorePlayer} : Computer ${scoreComputer}`);
+    if(scorePlayer > scoreComputer){
+        console.log('You are The Winner!');
+    }
+    else if(scorePlayer < scoreComputer){
+        console.log('Computer are The Winner!');
+    }
+    else (console.log('Tie! Amazing...'));
+}
+game();
